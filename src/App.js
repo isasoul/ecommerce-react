0e5d11c243a494533d209364a8a872c6bcbd94ac
Isabel Molina <mariaisabelmolinafer@gmail.com>
2023-05-07 16:29:37 -0300

    trabajando en la segunda entrega

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar';
+import Itemcount from  './components/ItemCount/ItemCount';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <ItemListContainer greeting={"El Placer de Conectar"}/>
-      
+      <Itemcount initial={1} stock={10} onAdd={(quantity) =>   console.log("Cantidad agregada", quantity)}/>
     </div>
   );
 }
 
 export default App;
+  
